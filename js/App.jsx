1f316f19bde7746d29b5340bcb98cbc90ba6c530
Julio Makdisse Saito <saitodisse@gmail.com>
2016-01-08 00:39:50 -0200

    limit 0..10

--- js/App.jsx
+++ js/App.jsx
@@ -1,6 +1,17 @@
 import { h, render } from 'yolk'
 
+
+// Yolk components inject a single object as an argument. The object has three
+// keys: props, children, and createEventHandler.
+// more info: https://github.com/garbles/yolk
 function Counter ({props, children, createEventHandler}) {
+
+  // createEventHandler
+  // ------------------
+  // Creates an exotic function that can also be used as an observable. If the
+  // function is called, the input value is pushed to the observable as it's
+  // latest value. In other words, when this function is used as an event
+  // handler, the result is an observable stream of events from that handler.
 
   // map all plus button click events to 1
   const handlePlus = createEventHandler()
@@ -11,10 +22,20 @@ function Counter ({props, children, createEventHandler}) {
   const minusOne$ = handleMinus.map(() => -1)
 
   // merge both event streams together and keep a running count of the result
-  const count$ = plusOne$.merge(minusOne$).scan((x, y) => x + y, 0).startWith(0)
+  const count$ = plusOne$
+  	.merge(minusOne$)
+	  .scan((x, y) => {
+	  	let total = x + y
+	  	if (total >= 0 && total <= 10) {
+	  		return total
+	  	} else {
+	  		return x
+	  	}
+	  }, 0)
+	  .startWith(0)
 
   // prop keys are always cast as observables
-  const title$ = props.title.map(title => `Awesome ${title}`)
+  const title$ = props.title.map(title => `Hello ${title}`)
 
   return (
     <div>
@@ -31,4 +52,4 @@ function Counter ({props, children, createEventHandler}) {
   )
 }
 
-render(<Counter title="Example" />, document.getElementById('container'))
+render(<Counter title="World" />, document.getElementById('container'))
---
+++ todo.md
@@ -0,0 +1,5 @@
+# TODO
+
+- [ ] limit 0..10
+- [ ] limit input-min..input-max
+- [ ] key binding (+) and (-)
